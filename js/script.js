97cc68c4b49c36e12a5287e505cbd264f4f15d25
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-02-24 17:42:44 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ const billAmount = document.querySelector('#bill-amount');
 const button = document.querySelectorAll('.button>button');
 const tipCustom = document.querySelector('#tip-custom');
 const numPeople = document.querySelector('#num-people');
-const tipP = document.querySelector('.tip-pp');
-const totalP = document.querySelector('.total-pp');
+const tipResult = document.querySelector('.tip-pp');
+const totalResult = document.querySelector('.total-pp');
 const reset = document.querySelector('.reset-button');
+
+let billTotal = 0.00;
+let customTipAmount = 0.00;
+let tipPercent = 0;
+let people = 0;
+let tipPP = 0.00;
+let totalPP = 0.00;
