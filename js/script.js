2e0212b3d1a6cb116e67f4c4024c45fd6168bb6e
João Paulo Menezes <joaopaulo.smenezes@hotmail.com>
2022-02-24 17:59:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const billAmount = document.querySelector('#bill-amount');
-const button = document.querySelectorAll('.button>button');
+const buttons = document.querySelectorAll('.button>button');
 const tipCustom = document.querySelector('#tip-custom');
 const numPeople = document.querySelector('#num-people');
 const tipResult = document.querySelector('.tip-pp');
 const totalResult = document.querySelector('.total-pp');
-const reset = document.querySelector('.reset-button');
+const resetButton = document.querySelector('.reset-button');
 
 let billTotal = 0.00;
 let customTipAmount = 0.00;
@@ -12,3 +12,13 @@ let tipPercent = 0;
 let people = 0;
 let tipPP = 0.00;
 let totalPP = 0.00;
+
+
+
+
+
+
+
+tipCustom.addEventListener('change',(e) => updateCustomTip(e));
+buttons.forEach(button => button.addEventListener('click',getTipPct));
+resetButton.addEventListener('click',resetForm);
